3ffdb4bbfd53b1a8d6f9c7d1b8aa3c6a43a36943
keidarb <keidar11@gmail.com>
2021-03-08 13:21:18 +0200

    Update k8s_backend_testing.py

--- k8s_backend_testing.py
+++ k8s_backend_testing.py
@@ -1,9 +1,9 @@
 import requests
 
 try:
-    f = open("k8s_url.txt", "r")
-    url = f.read()
-    print(url)
+    with open("k8s_url.txt", "r") as f:
+        url = f.read()
+        print(url)
     print(f'{url}/users/4444')
     res = requests.get(f'{url}/users/4444')
 
